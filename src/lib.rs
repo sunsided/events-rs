d04ee5a4405cc35c4dcdf01c024614a44c08edac
Markus Mayer <widemeadows@gmail.com>
2021-07-08 01:18:13 +0200

    Add deregistration through handler drops

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,84 @@
 use std::collections::BTreeMap;
+use std::sync::{Arc, RwLock, Weak};
 
-type EventHandlerDelegate = fn();
+type EventHandlerDelegate = dyn Fn();
+
+struct HandlerEntry {
+    pub handler: Box<EventHandlerDelegate>,
+}
+
+type MapInner = BTreeMap<usize, HandlerEntry>;
+type MapLocked = RwLock<MapInner>;
 
 pub struct EventHandler {
-    handlers: BTreeMap<usize, EventHandlerDelegate>,
+    handlers: Arc<MapLocked>,
+}
+
+/// A handle to a registration.
+/// When the handle is dropped, the registration is revoked.
+#[derive(Default)]
+pub struct Handle {
+    handle: Option<usize>,
+    pointer: Weak<MapLocked>,
+}
+
+impl Handle {
+    fn new(handle: Option<usize>, pointer: Weak<MapLocked>) -> Self {
+        Self { handle, pointer }
+    }
+
+    /// Determines whether the registration is still valid.
+    pub fn is_valid(&self) -> bool {
+        self.pointer.strong_count() > 0
+    }
+
+    /// Determines whether this an event was newly registered or
+    /// already existed.
+    pub fn is_new(&self) -> bool {
+        self.handle.is_some()
+    }
+}
+
+impl Drop for Handle {
+    fn drop(&mut self) {
+        if self.handle.is_none() {
+            return;
+        }
+
+        if let Some(map) = self.pointer.upgrade() {
+            let mut guard = map.write().unwrap();
+            guard.remove(&self.handle.unwrap());
+        }
+    }
 }
 
 impl EventHandler {
     pub fn new() -> Self {
         Self {
-            handlers: BTreeMap::new(),
+            handlers: Arc::new(MapLocked::new(MapInner::new())),
         }
     }
 
-    pub fn add(&mut self, handler: EventHandlerDelegate) -> bool {
-        let p_handler: usize = unsafe { *(handler as *const usize) };
-        match self.handlers.insert(p_handler, handler) {
-            None => true,
-            Some(_) => false,
-        }
+    pub fn len(&self) -> usize {
+        self.handlers.read().unwrap().len()
     }
 
-    pub fn remove(&mut self, handler: EventHandlerDelegate) -> bool {
-        let p_handler: usize = unsafe { *(handler as *const usize) };
-        match self.handlers.remove(&p_handler) {
-            Some(_) => true,
-            None => false,
+    #[must_use = "this handle must be held alive for as long as the event should be used"]
+    pub fn add(&mut self, handler: Box<EventHandlerDelegate>) -> Handle {
+        let p_handler: usize = (&handler as *const _) as usize;
+        let entry = HandlerEntry { handler };
+        let mut guard = self.handlers.write().unwrap();
+        let weak_ptr = Arc::downgrade(&self.handlers);
+        match guard.insert(p_handler, entry) {
+            None => Handle::new(Some(p_handler), weak_ptr),
+            Some(_) => Handle::new(None, weak_ptr),
         }
     }
 
     pub fn invoke(&self) {
-        for (_, handler) in self.handlers.iter() {
-            handler();
+        let guard = self.handlers.read().unwrap();
+        for (_, entry) in guard.iter() {
+            (entry.handler)();
         }
     }
 }
@@ -43,38 +90,59 @@ mod tests {
     fn dummy() {}
 
     #[test]
+    fn new_handler_has_no_registrations() {
+        let handler = EventHandler::new();
+        assert_eq!(handler.len(), 0);
+    }
+
+    #[test]
     fn can_add_unknown_handlers() {
         let mut handler = EventHandler::new();
-        assert_eq!(handler.add(dummy), true);
+        let handle = handler.add(Box::new(|| dummy()));
+        assert_eq!(handle.is_new(), true);
+        assert_eq!(handler.len(), 1);
     }
 
     #[test]
-    fn cannot_add_handlers_twice() {
+    fn can_remove_handlers() {
         let mut handler = EventHandler::new();
-        handler.add(dummy);
-        assert_eq!(handler.add(dummy), false);
+        let handle = handler.add(Box::new(|| dummy()));
+        assert_eq!(handler.len(), 1);
+        drop(handle);
+        assert_eq!(handler.len(), 0);
     }
 
     #[test]
-    fn cannot_remove_unknown_handlers() {
-        let mut handler = EventHandler::new();
-        assert_eq!(handler.remove(dummy), false);
-    }
+    #[allow(dead_code, unused_variables)]
+    fn it_works<'i>() {
+        let x = Box::new(0);
+        let y = Box::new(0);
+        let addr = *x as *const usize;
+        let addr2 = *x as *const usize;
+        let addr3 = *y as *const usize;
+        println!("{:?}", addr);
 
-    #[test]
-    fn can_remove_known_handlers() {
-        let mut handler = EventHandler::new();
-        handler.add(dummy);
-        assert_eq!(handler.remove(dummy), true);
-        assert_eq!(handler.remove(dummy), false);
-    }
-
-    #[test]
-    fn it_works() {
+        /*
         let mut handler = EventHandler::new();
         let mut value = 0;
-        handler.add(|| value += 1);
+        let value_ref: &'i mut i32 = &mut value;
+
+        struct State<'a> {
+            pub value: &'a mut i32,
+        }
+
+        fn dummy(state: &EventStateType) {
+            let state: Box<dyn Any> = state.unwrap();
+            let state = state.downcast_ref::<State>().unwrap();
+            (*state.value) += 1;
+        }
+
+        let state = EventStateType::Some(Box::new(State { value: value_ref }));
+        handler.add(dummy, state);
+
         handler.invoke();
+
         assert_eq!(value, 1);
+        */
     }
 }
